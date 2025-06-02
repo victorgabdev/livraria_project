8a4c96e6c61761e8c7d97c78c45ccb5ace9ac2b3
victorgabdev <victorgabdev@gmail.com>
2025-06-02 10:37:57 -0300

    exceptions

--- livraria/src/livraria/MeuPrimeiroPrograma.java
+++ livraria/src/livraria/MeuPrimeiroPrograma.java
@@ -1,10 +1,28 @@
 package livraria;
 
+import java.io.FileNotFoundException;
+
 public class MeuPrimeiroPrograma {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("O primeiro de muitos!");
-
+		
+		// Fully qualified name
+		//new java.io.FileInputStream("arquivo.txt");
+		
+		/*
+		try {
+			new java.io.FileInputStream("arquivo.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("Não consegui abrir o arquivo.");
+		}
+		*/
+		
+		abreArquivo();
+	}
+	
+	public static void abreArquivo() throws FileNotFoundException {
+		new java.io.FileInputStream("arquivo.txt");	// Fully Qualified Name
 	}
 
 }
